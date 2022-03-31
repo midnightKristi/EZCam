dbe2434da8661fae5b0f07e9c688c5bcc482f96e
midnightKristi <69993761+midnightKristi@users.noreply.github.com>
2022-03-30 20:17:44 -0400

    Add files via upload

---
+++ WemoSwitch.h
@@ -0,0 +1,39 @@
+#ifndef WemoSwitch_H
+#define WemoSwitch_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiUdp.h>
+#include "CallbackFunction.h"
+
+
+class WemoSwitch {
+private:
+        WebServer *server = NULL;
+        WiFiUDP UDP;
+        String serial;
+        String persistent_uuid;
+        String device_name;
+        unsigned int localPort;
+        String deviceStatus;
+        CallbackFunction onCallback;
+        CallbackFunction offCallback;
+
+        void startWebServer();
+        void handleEventservice();
+        void handleUpnpControl();
+        void handleRoot();
+        void handleSetupXml();
+public:
+        WemoSwitch();
+        WemoSwitch(String alexaInvokeName, unsigned int port, CallbackFunction onCallback, CallbackFunction offCallback);
+        ~WemoSwitch();
+        String getAlexaInvokeName();
+        void serverLoop();
+        void respondToSearch(IPAddress& senderIP, unsigned int senderPort);
+        void turnOff() { deviceStatus = "0"; }
+        void turnOn() { deviceStatus = "1"; }
+};
+
+#endif
